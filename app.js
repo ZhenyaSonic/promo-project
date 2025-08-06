de9173207705320b658733bbf418f0a3f9e9443b
Евгений Братанов <sonik.zhenyaa@gmail.com>
2025-08-06 19:57:27 +0300

    Оптимизировал html через js

---
+++ app.js
@@ -0,0 +1,40 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const promoData = [
+        { title: "Скидка 10% для пенсионеров", description: "Новая акция для наших гостей пенсионного возраста", imgSrc: "img/picture-offers.png" },
+        { title: "Скидка 10% для пенсионеров", description: "Новая акция для наших гостей пенсионного возраста", imgSrc: "img/picture-offers.png" },
+        { title: "Скидка 10% для пенсионеров", description: "Новая акция для наших гостей пенсионного возраста", imgSrc: "img/picture-offers.png" },
+        { title: "Скидка 10% для пенсионеров", description: "Новая акция для наших гостей пенсионного возраста", imgSrc: "img/picture-offers.png" },
+        // Эти 2 карточки должны быть видны только на планшетах
+        { title: "Скидка 10% для пенсионеров", description: "Новая акция для наших гостей пенсионного возраста", imgSrc: "img/picture-offers.png", isTabletOnly: true },
+        { title: "Скидка 10% для пенсионеров", description: "Новая акция для наших гостей пенсионного возраста", imgSrc: "img/picture-offers.png", isTabletOnly: true },
+    ];
+
+    // Контейнер, куда будем вставлять карточки
+    const promoGrid = document.getElementById("promo-grid");
+
+    promoData.forEach((item, index) => {
+        const visibilityClasses = item.isTabletOnly 
+            ? 'd-none d-md-block d-lg-none' 
+            : '';
+
+        const cardHTML = `
+            <div class="col-12 col-sm-6 col-md-4 col-lg-3 ${visibilityClasses}">
+                <a href="#" class="promo-card">
+                    <img src="${item.imgSrc}" alt="${item.title}" class="promo-card-img">
+                    <div class="promo-card-body">
+                        <h3 class="promo-card-title">${item.title}</h3>
+                        <p class="promo-card-text">${item.description}</p>
+                    </div>
+                    <div class="promo-card-arrow-wrapper">
+                        <div class="promo-card-arrow">
+                            <img src="img/vector-offers.svg" alt="Перейти">
+                        </div>
+                    </div>
+                </a>
+            </div>
+        `;
+
+        promoGrid.innerHTML += cardHTML;
+    });
+});
